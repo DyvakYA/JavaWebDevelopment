f66290b55b41b790469cfad952ae68659f17d07f
DyvakYA <dyvakyurii@gmail.com>
2016-10-30 10:22:17 +0200

    Bad testing, definitely fail

---
+++ TheHigherLowerGame/test/com/dyvak/ControllerTest.java
@@ -0,0 +1,18 @@
+package com.dyvak;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ControllerTest {
+
+    @Test
+    public void testProcessUser() throws Exception {
+
+    }
+
+    @Test
+    public void testCheckCurrentValue() throws Exception {
+
+    }
+}
---
+++ TheHigherLowerGame/test/com/dyvak/ModelTest.java
@@ -0,0 +1,13 @@
+package com.dyvak;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ModelTest {
+
+    @Test
+    public void testCheckResult() throws Exception {
+
+    }
+}
